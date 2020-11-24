b590077bc30d88e5329b90be3bd028306085b6a4
jin0008 <jin0008@hanmail.net>
2020-11-24 18:01:56 +0900

    Add files via upload

---
+++ RP1_AluYdetection_revised.py
@@ -0,0 +1,22 @@
+
+#!/usr/bin/env python
+import os
+
+os.system("find *.fastq.gz -type f | parallel -j+1 zgrep -c -e ACCGCGCCCGGCCGTGTTTTCTTTGG -e CCAAAGAAAACACGGCCGGGCGCGGT \
+| awk '{sum += $1} END {print sum}' > mutantcount")
+os.system("find *.fastq.gz -type f | parallel -j+1 zgrep -c -e GTTATCAGTATATGTGTTTTCTTTGG -e CCAAAGAAAACACATATACTGATAAC \
+| awk '{sum += $1} END {print sum}' > wildtypecount")
+
+f1=open("mutantcount", "r")
+f2=open("wildtypecount", "r")
+x=float(f1.read())
+y=float(f2.read())
+z=x/(x+y)
+print(z)
+
+if z < 0.1 :
+	print('no AluY insertion in RP1 exon4 was detect')
+elif z >=0.3 :
+	print('AluY inserion in RP1 exon4 was detected!!')
+else :
+	print('AluY insertion in RP1 exon 4 was suspected!! please confirm the AluY in RP1 exon4 by integrative genomic viewer')
